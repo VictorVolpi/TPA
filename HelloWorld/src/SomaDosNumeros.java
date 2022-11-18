f86fe98ed07e925623866c1aa545230cd3562d16
Victor Volpi <112173184+VictorVolpi@users.noreply.github.com>
2022-11-18 08:34:45 -0300

    Hello World

---
+++ HelloWorld/src/SomaDosNumeros.java
@@ -0,0 +1,10 @@
+
+public class SomaDosNumeros {
+	public static void main (String [] args) {
+		double n1, n2, soma;
+		n1=10;
+		n2=5;
+		soma = n1+n2;
+		System.out.println("A soma = "+ soma);
+	}
+}
