946692d21d0247548b72ca4ea0e59fa778c85b3a
Victor Volpi <112173184+VictorVolpi@users.noreply.github.com>
2022-11-18 21:38:43 -0300

    FOR

---
+++ FOR/Mutiplodedez.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Mutiplodedez {
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%10==0) {
+				System.out.println(i+" - Multiplo de 10");
+			}
+		}
+	}
+}
